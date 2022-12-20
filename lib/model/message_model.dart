488237a7fbcc9f161e8733b6ebea4ca89e37f5cc
Ykwang <kohyungkwang@gmail.com>
2022-12-21 01:18:17 +0800

    Create message screen

---
+++ lib/model/message_model.dart
@@ -0,0 +1,8 @@
+class Messagedetail {
+  String image, name, desc, click;
+  Messagedetail(
+      {required this.image,
+      required this.name,
+      required this.desc,
+      required this.click});
+}
