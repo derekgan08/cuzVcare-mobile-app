8c958a3e0a893213c92560ce3c76ab7ac525c4bc
Derek Gan <187380048+derekgan08@users.noreply.github.com>
2023-01-03 00:58:09 +0800

    Finalise data controller

---
+++ lib/controller/data_controller.dart
@@ -0,0 +1,224 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:path/path.dart' as pathv;
+
+class DataController extends GetxController {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  DocumentSnapshot? myDocument;
+
+  var allUsers = <DocumentSnapshot>[].obs;
+  var filteredUsers = <DocumentSnapshot>[].obs;
+  var allEvents = <DocumentSnapshot>[].obs;
+  var filteredEvents = <DocumentSnapshot>[].obs;
+  var joinedEvents = <DocumentSnapshot>[].obs;
+  var savesEvents = <DocumentSnapshot>[].obs;
+
+  var allRewards = <DocumentSnapshot>[].obs;
+  var filteredRewards = <DocumentSnapshot>[].obs;
+  var redempRewards = <DocumentSnapshot>[].obs;
+
+  var isEventsLoading = false.obs;
+  var isRewardsLoading = false.obs;
+
+  var isMessageSending = false.obs;
+  sendMessageToFirebase(
+      {Map<String, dynamic>? data, String? lastMessage, String? grouid}) async {
+    isMessageSending(true);
+
+    await FirebaseFirestore.instance
+        .collection('chats')
+        .doc(grouid)
+        .collection('chatroom')
+        .add(data!);
+    await FirebaseFirestore.instance.collection('chats').doc(grouid).set({
+      'lastMessage': lastMessage,
+      'groupId': grouid,
+      'group': grouid!.split('-'),
+    }, SetOptions(merge: true));
+
+    isMessageSending(false);
+  }
+
+  createNotification(String recUid) {
+    FirebaseFirestore.instance
+        .collection('notifications')
+        .doc(recUid)
+        .collection('myNotifications')
+        .add({
+      'message': "\tSend you a message.",
+      'image': myDocument!.get('image'),
+      'name': myDocument!.get('first') + " " + myDocument!.get('last'),
+      'time': DateTime.now()
+    });
+  }
+
+  // Fetch all users and events
+  getMyDocument() {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .snapshots()
+        .listen((event) {
+      myDocument = event;
+    });
+  }
+
+  Future<String> uploadImageToFirebase(File file) async {
+    String fileUrl = '';
+    String fileName = pathv.basename(file.path);
+    var reference = FirebaseStorage.instance.ref().child('myfiles/$fileName');
+    UploadTask uploadTask = reference.putFile(file);
+    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+    await taskSnapshot.ref.getDownloadURL().then((value) {
+      fileUrl = value;
+    });
+    print("Url $fileUrl");
+    return fileUrl;
+  }
+
+  Future<String> uploadThumbnailToFirebase(Uint8List file) async {
+    String fileUrl = '';
+    String fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    var reference =
+        FirebaseStorage.instance.ref().child('myfiles/$fileName.jpg');
+    UploadTask uploadTask = reference.putData(file);
+    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+    await taskSnapshot.ref.getDownloadURL().then((value) {
+      fileUrl = value;
+    });
+
+    print("Thumbnail $fileUrl");
+
+    return fileUrl;
+  }
+
+  Future<bool> createEvent(Map<String, dynamic> eventData) async {
+    bool isCompleted = false;
+
+    await FirebaseFirestore.instance
+        .collection('events')
+        .add(eventData)
+        .then((value) {
+      isCompleted = true;
+      Get.snackbar('Event Uploaded', 'Event is uploaded successfully.',
+          colorText: Colors.white, backgroundColor: Colors.blue);
+    }).catchError((e) {
+      isCompleted = false;
+    });
+
+    return isCompleted;
+  }
+
+  Future<bool> createReward(Map<String, dynamic> rewardData) async {
+    bool isCompleted = false;
+
+    await FirebaseFirestore.instance
+        .collection('rewards')
+        .add(rewardData)
+        .then((value) {
+      isCompleted = true;
+      Get.snackbar('Reward Uploaded', 'Reward is uploaded successfully.',
+          colorText: Colors.white, backgroundColor: Colors.blue);
+    }).catchError((e) {
+      isCompleted = false;
+    });
+
+    return isCompleted;
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    getMyDocument();
+    getUsers();
+    getEvents();
+    getBookmarkEvents();
+    getRewards();
+    getRedempRewards();
+  }
+
+  var isUsersLoading = false.obs;
+
+  getUsers() {
+    isUsersLoading(true);
+    FirebaseFirestore.instance.collection('users').snapshots().listen((event) {
+      allUsers.value = event.docs;
+      filteredUsers.value.assignAll(allUsers);
+      isUsersLoading(false);
+    });
+  }
+
+  getEvents() {
+    isEventsLoading(true);
+
+    FirebaseFirestore.instance.collection('events').snapshots().listen((event) {
+      allEvents.assignAll(event.docs);
+      filteredEvents.assignAll(event.docs);
+
+      joinedEvents.value = allEvents.where((e) {
+        List joinedIds = e.get('joined');
+
+        return joinedIds.contains(FirebaseAuth.instance.currentUser!.uid);
+      }).toList();
+
+      isEventsLoading(false);
+    });
+  }
+
+  getBookmarkEvents() {
+    isEventsLoading(true);
+
+    FirebaseFirestore.instance.collection('events').snapshots().listen((event) {
+      allEvents.assignAll(event.docs);
+      filteredEvents.assignAll(event.docs);
+
+      savesEvents.value = allEvents.where((e) {
+        List savesIds = e.get('saves');
+
+        return savesIds.contains(FirebaseAuth.instance.currentUser!.uid);
+      }).toList();
+
+      isEventsLoading(false);
+    });
+  }
+
+  getRewards() {
+    isRewardsLoading(true);
+
+    FirebaseFirestore.instance
+        .collection('rewards')
+        .snapshots()
+        .listen((reward) {
+      allRewards.assignAll(reward.docs);
+
+      isRewardsLoading(false);
+    });
+  }
+
+  getRedempRewards() {
+    isRewardsLoading(true);
+
+    FirebaseFirestore.instance
+        .collection('rewards')
+        .snapshots()
+        .listen((reward) {
+      allRewards.assignAll(reward.docs);
+      filteredRewards.assignAll(reward.docs);
+
+      redempRewards.value = allRewards.where((e) {
+        List redeemIds = e.get('redeemed');
+
+        return redeemIds.contains(FirebaseAuth.instance.currentUser!.uid);
+      }).toList();
+
+      isRewardsLoading(false);
+    });
+  }
+}
